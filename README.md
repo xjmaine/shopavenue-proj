d06d461ef1c91b54ca9ba0b5557a9d73292bfa53
Freddie <xjfreddie@gmail.com>
2022-09-27 12:32:47 +0000

    Initial render

---
+++ README.md
@@ -0,0 +1,1 @@
+#shopavenue-project
---
+++ src/test/java/io/avenue/ShopAvenueApplicationTests.java
@@ -0,0 +1,13 @@
+package io.avenue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAvenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
