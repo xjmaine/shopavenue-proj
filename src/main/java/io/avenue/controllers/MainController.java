7c4418107b73d49a4336c6b4eba890dca798f37d
Freddie <xjfreddie@gmail.com>
2022-09-27 14:34:18 +0000

    initial release

--- src/main/java/io/avenue/controllers/MainController.java
+++ src/main/java/io/avenue/controllers/MainController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/index")
 public class MainController {
 	
-	@GetMapping("/")
+	@GetMapping
 	public String mainIdex() {
 		return "index";
 	}
